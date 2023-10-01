8dbbe9815bdb238e6002e8cf5bfb6b9b7e1dfa0a
Carlos <githubjuanmanuel@gmail.com>
2023-10-01 16:01:26 -0500

    nueva tarea

---
+++ Tareas_Clase/Tarea7/js/app.js
@@ -0,0 +1,30 @@
+const Header = document.createElement('header');
+const Main = document.createElement('main')
+
+const appHeader = `<header >
+<h1>Tarea n°7</h1>
+<p>Proyecto pagina <span>Js</span></p>
+</header>`
+
+const appMain = `<main>
+<h3>Este proyecto involucra</h3>
+<section>
+    <div>
+        <img src="sources/img1.webp" alt="Imagen 1">
+    </div>
+    <div>
+        <p>Estos son algunos lenguaje básicos para crear pagínas web</p>
+        <ul>
+            <li>Html:<span> Se utiliza para crear la estructura</span><li>
+            <li>Css: <span>Es usado para darle estilos</span><li>
+            <li>JavaScript: <span>Se usa para darle interactividad</span><li>
+        </ul>
+    </div>
+</section>
+</main>`
+
+Header.innerHTML = appHeader;
+Main.innerHTML = appMain;
+
+document.body.appendChild(Header);
+document.body.appendChild(Main);
